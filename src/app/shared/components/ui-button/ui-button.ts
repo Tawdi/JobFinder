c7bad17632b4e2b8d6e9ee08f35f95b8fb1b5618
Tawdi <ahmedtawdi00@gmail.com>
2026-02-09 14:04:20 +0100

    add generic ui button

---
+++ src/app/shared/components/ui-button/ui-button.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'ui-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui-button.html',
+  styleUrl: './ui-button.css',
+})
+export class UiButton {
+  @Input() variant: 'primary' | 'secondary' | 'outline' | 'accent' = 'primary';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() fullWidth = false;
+  @Input() disabled = false;
+  @Input() loading = false;
+}
