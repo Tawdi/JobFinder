78420977d1de73e7904f6ebcd0daad9a64d9fbb8
Tawdi <ahmedtawdi00@gmail.com>
2026-02-09 16:49:02 +0100

    add Login page

---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UiInput} from '../../../shared/components/ui-input/ui-input';
+import {UiButton} from '../../../shared/components/ui-button/ui-button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, UiInput, UiButton],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+form :any
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }
+    )
+  }
+
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get password() {
+    return this.form.controls.password;
+  }
+}
