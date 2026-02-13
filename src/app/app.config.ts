c096f0784b286fff8a17a0435461f43a6272047c
Tawdi <ahmedtawdi00@gmail.com>
2026-02-13 11:59:59 +0100

    add ngrx dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import {jobsReducer} from './core/store/reducers/jobs.reducer';
+import {JobsEffects} from './core/store/effects/jobs.effect';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore({
+        jobs: jobsReducer,
+    }),
+    provideEffects(JobsEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode() ,
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    })
+]
 };
